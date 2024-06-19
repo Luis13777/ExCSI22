7d3944200ef9c4f3deaff50113bae7fd4ce16057
Luan-Horta <luanhorta16@gmail.com>
2024-06-19 00:33:54 -0300

    mudança na main: escolha de sabores, adição de tratamento de erros de input, adição de novos ingredientes

--- PizzaPython.py
+++ PizzaPython.py
@@ -3,119 +3,230 @@ from abc import ABC, abstractmethod
 # Componente (Classe Base Abstrata)
 class Pizza(ABC):
     @abstractmethod
-    def get_custo(self):
+    def get_cost(self):
         pass
 
     @abstractmethod
-    def get_descricao(self):
+    def get_ingredients(self):
+        pass
+
+    @abstractmethod
+    def get_name(self):
+        pass
+
+    @abstractmethod
+    def get_type(self):
         pass
 
 # ComponenteConcreto (Implementação da Classe Base)
-class Massa(Pizza):
-    def get_custo(self):
-        return 10.0
+class Base(Pizza):
+    def __init__(self, pType, name):
+        super().__init__()
+        self._type = pType
+        self._name = name
+    
+    def get_cost(self):
+        return 45.0
 
-    def get_descricao(self):
-        return "Massa da Pizza"
+    def get_ingredients(self):
+        if self._type == "salgada":
+            return "Molho de Tomate"
+        else:
+            return ""
+    
+    def get_name(self):
+        return self._name
+
+    def get_type(self):
+        return self._type
 
 # Decorador (Classe Base)
-class IngredienteDecorator(Pizza):
+class ingredientDecorator(Pizza):
     def __init__(self, pizza):
         self._decorated_pizza = pizza
 
-    def get_custo(self):
-        return self._decorated_pizza.get_custo()
+    def get_cost(self):
+        return self._decorated_pizza.get_cost()
 
-    def get_descricao(self):
-        return self._decorated_pizza.get_descricao()
+    def get_ingredients(self):
+        return self._decorated_pizza.get_ingredients()
+    
+    def get_name(self):
+        return self._decorated_pizza.get_name()
+
+    def get_type(self):
+        return self._decorated_pizza.get_type()
 
 # Decoradores Concretos
-class QueijoDecorator(IngredienteDecorator):
-    def get_custo(self):
-        return self._decorated_pizza.get_custo() + 2.0
+class Queijo(ingredientDecorator):
+    def get_cost(self):
+        return self._decorated_pizza.get_cost() + 4.0
 
-    def get_descricao(self):
-        return self._decorated_pizza.get_descricao() + ", Queijo"
+    def get_ingredients(self):
+        return self._decorated_pizza.get_ingredients() + ", Queijo"
 
-class CalabresaDecorator(IngredienteDecorator):
-    def get_custo(self):
-        return self._decorated_pizza.get_custo() + 3.0
+class Calabresa(ingredientDecorator):
+    def get_cost(self):
+        return self._decorated_pizza.get_cost() + 5.0
 
-    def get_descricao(self):
-        return self._decorated_pizza.get_descricao() + ", Calabresa"
+    def get_ingredients(self):
+        return self._decorated_pizza.get_ingredients() + ", Calabresa"
 
-class FrangoDecorator(IngredienteDecorator):
-    def get_custo(self):
-        return self._decorated_pizza.get_custo() + 4.0
+class Frango(ingredientDecorator):
+    def get_cost(self):
+        return self._decorated_pizza.get_cost() + 5.0
 
-    def get_descricao(self):
-        return self._decorated_pizza.get_descricao() + ", Frango"
+    def get_ingredients(self):
+        return self._decorated_pizza.get_ingredients() + ", Frango"
+    
+class Presunto(ingredientDecorator):
+    def get_cost(self):
+        return self._decorated_pizza.get_cost() + 3.0
 
-class TomateDecorator(IngredienteDecorator):
-    def get_custo(self):
-        return self._decorated_pizza.get_custo() + 1.0
+    def get_ingredients(self):
+        return self._decorated_pizza.get_ingredients() + ", Presunto"
+    
+class Catupiry(ingredientDecorator):
+    def get_cost(self):
+        return self._decorated_pizza.get_cost() + 2.0
 
-    def get_descricao(self):
-        return self._decorated_pizza.get_descricao() + ", Tomate"
+    def get_ingredients(self):
+        return self._decorated_pizza.get_ingredients() + ", Catupiry"
+    
+class Ovo(ingredientDecorator):
+    def get_cost(self):
+        return self._decorated_pizza.get_cost() + 1.0
 
-class CebolaDecorator(IngredienteDecorator):
-    def get_custo(self):
-        return self._decorated_pizza.get_custo() + 0.5
+    def get_ingredients(self):
+        return self._decorated_pizza.get_ingredients() + ", Ovo"
 
-    def get_descricao(self):
-        return self._decorated_pizza.get_descricao() + ", Cebola"
+class Tomate(ingredientDecorator):
+    def get_cost(self):
+        return self._decorated_pizza.get_cost()
 
-class AzeitonaDecorator(IngredienteDecorator):
-    def get_custo(self):
-        return self._decorated_pizza.get_custo() + 1.5
+    def get_ingredients(self):
+        return self._decorated_pizza.get_ingredients() + ", Tomate"
 
-    def get_descricao(self):
-        return self._decorated_pizza.get_descricao() + ", Azeitona"
+class Cebola(ingredientDecorator):
+    def get_cost(self):
+        return self._decorated_pizza.get_cost() 
 
-# Interface de Terminal
-def main():
-    pizza = Massa()
-    print("--------------------------------------------------")
-    print("Bem vindo a Pizzaria em Python!")
-    print("--------------------------------------------------")
+    def get_ingredients(self):
+        return self._decorated_pizza.get_ingredients() + ", Cebola"
+
+class Manjericao(ingredientDecorator):
+    def get_cost(self):
+        return self._decorated_pizza.get_cost()
+
+    def get_ingredients(self):
+        return self._decorated_pizza.get_ingredients() + ", Manjericão"
+    
+class Azeitona(ingredientDecorator):
+    def get_cost(self):
+        return self._decorated_pizza.get_cost()
+
+    def get_ingredients(self):
+        return self._decorated_pizza.get_ingredients() + ", Azeitona"
+
+class Chocolate(ingredientDecorator):
+    def get_cost(self):
+        return self._decorated_pizza.get_cost() + 4.0
+
+    def get_ingredients(self):
+        return self._decorated_pizza.get_ingredients() + "Chocolate ao Leite"
+
+class Morango(ingredientDecorator):
+    def get_cost(self):
+        return self._decorated_pizza.get_cost() + 3.0
+
+    def get_ingredients(self):
+        return self._decorated_pizza.get_ingredients() + ", Morango"
+
+class MM(ingredientDecorator):
+    def get_cost(self):
+        return self._decorated_pizza.get_cost() + 1.0
+
+    def get_ingredients(self):
+        return self._decorated_pizza.get_ingredients() + ", MM"
+          
+
+# interação com o usuário
+
+def select_pizza():
+    pizzas = [Azeitona(Manjericao(Tomate(Queijo(Base("salgada", "Marguerita"))))),
+              Azeitona(Cebola(Calabresa(Base("salgada", "Calabresa")))),
+              Azeitona(Catupiry(Frango(Queijo(Base("salgada", "Frango"))))),
+              Azeitona(Cebola(Ovo(Presunto(Queijo(Base("salgada", "Portuguesa")))))),
+              Morango(Chocolate(Base("doce", "Chocolate com Morango"))),
+              MM(Chocolate(Base("doce", "Chocolate com MM")))
+              ]
+    
+    print("\nSelecione uma opção:")
+    k = 1
+    for pizza in pizzas:
+        print(str(k) + ". " + pizza.get_name() + " (" + pizza.get_ingredients() + ") R$ " + str(pizza.get_cost()))
+        k += 1
 
     while True:
-        print("\nPizza atual: ", pizza.get_descricao())
-        print("Custo atual: R$", pizza.get_custo())
-        print("\nEscolha uma opção:")
-        print("1. Adicionar Queijo (R$ 2.0)")
-        print("2. Adicionar Calabresa (R$ 3.0)")
-        print("3. Adicionar Frango (R$ 4.0)")
-        print("4. Adicionar Tomate (R$ 1.0)")
-        print("5. Adicionar Cebola (R$ 0.5)")
-        print("6. Adicionar Azeitona (R$ 1.5)")
-        print("7. Finalizar pedido")
-        print("8. Sair")
-        
-        opcao = input("Opção: ")
-        
-        if opcao == "1":
-            pizza = QueijoDecorator(pizza)
-        elif opcao == "2":
-            pizza = CalabresaDecorator(pizza)
-        elif opcao == "3":
-            pizza = FrangoDecorator(pizza)
-        elif opcao == "4":
-            pizza = TomateDecorator(pizza)
-        elif opcao == "5":
-            pizza = CebolaDecorator(pizza)
-        elif opcao == "6":
-            pizza = AzeitonaDecorator(pizza)
-        elif opcao == "7":
-            print("\nPedido finalizado!")
-            print("Pizza: ", pizza.get_descricao())
-            print("Custo total: R$", pizza.get_custo())
-            break
-        elif opcao == "8":
-            print("Saindo...")
-            break
-        else:
-            print("Opção inválida! Tente novamente.")
+        try:
+            option = int(input("Opção: "))
+
+            if option >= 1 and option <= len(pizzas):
+                name = pizzas[option - 1].get_name()
+                cost = pizzas[option - 1].get_cost()
+                return name, cost
+            else:
+                print("Opção não existe! Tente novamente.")
+
+        except (ValueError, TypeError):
+            print("Entrada inválida! Tente novamente.")
+
+
+def main():
+    print("--------------------------------------------------")
+    print("Bem vindo(a) à Pizzaria em Python!")
+    print("--------------------------------------------------")
+    pizzaList = ''
+    totalCost = 0.0
+
+    while True:
+        print("\npizzas adicionadas: ", pizzaList)
+        print("preço: R$ ", totalCost)
+      
+        print("\nSelecione uma opção:")
+        print("1. Adicionar Pizza")
+        print("2. Finalizar pedido")
+        print("3. Sair")   
+
+        while True:
+            try:
+                option = int(input("Opção: "))
+
+                if option == 1:
+                    try:
+                        name, cost = select_pizza()
+                        pizzaList += name + ". "
+                        totalCost += cost       
+                    except Exception as error:
+                        print(f'{error}')
+                    break
+
+                    
+                elif option == 2:
+                    print("\nPedido finalizado!")
+                    print("pizzas: ", pizzaList)
+                    print("preço: R$", totalCost)
+                    return
+                elif option == 3:
+                    print("Saindo...")
+                    return
+                else:
+                    print("Opção não existe! Tente novamente.")
+                
+            except (ValueError, TypeError):
+                print("Entrada inválida! Tente novamente.")
+
+
 
 if __name__ == "__main__":
     main()
